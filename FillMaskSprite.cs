066ce79b0b35c48fa6cad0b7fe0df16bfbe30377
AlexStraga87 <59977788+AlexStraga87@users.noreply.github.com>
2020-11-17 17:29:48 +0300

    Add files via upload

---
+++ FillMaskSprite.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Linq;
+using System;
+using Game;
+
+public class FillMaskSprite : EditorWindow
+{
+    [MenuItem("Tools/Fill all Sprite Mask")]
+    public static void FillAllSpriteMasks()
+    {
+        List<GameObject> gameObjects = GetAllPrefabs();
+        foreach (var gameObject in gameObjects)
+        {
+            if (FillMask(gameObject))
+            {
+                EditorUtility.SetDirty(gameObject);
+                if (PrefabUtility.GetCorrespondingObjectFromSource(gameObject) != null)
+                    PrefabUtility.ApplyPrefabInstance(gameObject, InteractionMode.AutomatedAction);
+            }
+        }
+    }
+
+    // Получение всех префабов в проекте, кроме Variant префабов
+    public static List<GameObject> GetAllPrefabs()
+    {
+        string[] interactionGUIDs = AssetDatabase.FindAssets("t:Prefab");
+        List<GameObject> list = new List<GameObject>();
+        foreach (string guid2 in interactionGUIDs)
+        {
+            GameObject gameObject = AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(guid2)) as GameObject;
+            if (PrefabUtility.GetPrefabAssetType(gameObject) != PrefabAssetType.Variant)
+                list.Add(gameObject);
+        }
+        return list;
+    }
+
+    // Получаем всех детей и сам корневой gameObject 
+    private static List<GameObject> GetAllGameObject(GameObject root)
+    {
+        List<GameObject> result = new List<GameObject>();
+        result.Add(root);
+        if (root.transform.childCount > 0)
+        {
+            foreach (Transform VARIABLE in root.transform)
+            {
+                Searcher(result, VARIABLE.gameObject);
+            }
+        }
+        return result;
+    }
+
+    // Перебираем всех детей
+    private static void Searcher(List<GameObject> list, GameObject root)
+    {
+        list.Add(root);
+        if (root.transform.childCount > 0)
+        {
+            foreach (Transform VARIABLE in root.transform)
+            {
+                Searcher(list, VARIABLE.gameObject);
+            }
+        }
+    }
+
+    // Заполняем Маски объектам и их детям если они есть
+    private static bool FillMask(GameObject gameObject)
+    {
+        bool isChanged = false;
+        List<GameObject> childs = GetAllGameObject(gameObject);
+
+        foreach (var child in childs)
+        {
+            var spriteMaskChangeRuntime = child.GetComponent<SpriteMaskChangeRuntime>();
+            var spriteRenderer = child.GetComponent<SpriteRenderer>();
+            var spriteMask = child.GetComponent<SpriteMask>();
+
+            if (spriteMaskChangeRuntime)
+            {
+                FillMask(spriteMaskChangeRuntime);
+                isChanged = true;
+            }
+            if (spriteRenderer && spriteMask)
+            {
+                FillMask(spriteRenderer, spriteMask);
+                isChanged = true;
+            }
+        }
+
+        return isChanged;
+    }
+
+    private static void FillMask(SpriteMaskChangeRuntime spriteMaskChangeRuntime)
+    {
+        var serObject = new SerializedObject(spriteMaskChangeRuntime);
+
+        var sprites = serObject.FindProperty("_sprites");
+
+        var mask = serObject.FindProperty("_masks");
+        mask.ClearArray();
+
+        var spriteList = new List<Sprite>();
+        for (int i = 0; i < sprites.arraySize; i++)
+        {
+            spriteList.Add((Sprite)sprites.GetArrayElementAtIndex(i).objectReferenceValue);
+        }
+
+        for (int i = 0; i < spriteList.Count; i++)
+        {
+            mask.InsertArrayElementAtIndex(i);
+            //if (spriteList[i] != null)
+            mask.GetArrayElementAtIndex(i).objectReferenceValue = GetMaskForCurrentSprite(spriteList[i]);
+        }
+
+        serObject.ApplyModifiedProperties();
+        PrefabUtility.RecordPrefabInstancePropertyModifications(spriteMaskChangeRuntime);
+    }
+
+    private static void FillMask(SpriteRenderer spriteRenderer, SpriteMask spriteMask)
+    {
+        spriteMask.sprite = GetMaskForCurrentSprite(spriteRenderer.sprite);
+        PrefabUtility.RecordPrefabInstancePropertyModifications(spriteMask);
+    }
+
+    // Основываясь на спрайте ищем ему подходящую маску
+    private static Sprite GetMaskForCurrentSprite(Sprite sprite)
+    {
+        if (sprite == null)
+            return null;
+        
+        if (sprite.name.Contains("_xxx"))
+        {
+            Debug.LogWarning(sprite.name + " не требуется маска");
+            return null;
+        }
+
+        string path = AssetDatabase.GetAssetPath(sprite); 
+        path = path.Insert(path.Length - 4, "_MASK");
+        Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>().ToArray();
+
+        foreach (var currentSprite in sprites)
+        {
+            if (currentSprite.name == sprite.name)
+            {
+                return currentSprite;
+            }
+        }
+        Debug.LogWarning("Не могу найти файл " + path);
+        return null;
+    }
+
+}
+
---
+++ SpriteMaskChangeRuntime.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+namespace Game
+{
+    [AddComponentMenu(nameof(Game) + "/" + nameof(SpriteMaskChangeRuntime))]
+    [RequireComponent(typeof(SpriteRenderer), typeof(SpriteMask))]
+    public class SpriteMaskChangeRuntime : MonoBehaviour
+    {
+        [Tooltip("будет ли анимация сама запускаться при старте")]
+        public bool StartSelfRun;
+
+        [Tooltip("будет ли анимация зацикливаться")]
+        public bool Loop;
+
+        [Tooltip("Задержка анимации")] [SerializeField]
+        private float TimeDelay;
+        
+        public UnityAction<int> OnAnimationUpdate;
+
+        [SerializeField] private float _speed;
+
+        [Tooltip("Основная анимация")] [SerializeField]
+        private Sprite[] _sprites, _masks;
+
+        private SpriteRenderer _render;
+        private Coroutine _timer, _timerStart;
+        private SpriteMask _mask;
+        
+        private int _index;
+        private bool _isSelfRestart;
+
+        private int index
+        {
+            get => _index;
+            set
+            {
+                OnAnimationUpdate?.Invoke(index);
+                _index = value;
+            }
+        }
+
+
+        protected virtual void Awake()
+        {
+            _render = GetComponent<SpriteRenderer>();
+            _mask = GetComponent<SpriteMask>();
+        }
+
+        private void Start()
+        {
+            if (StartSelfRun == true)
+                Play();
+        }
+
+        private void OnEnable()
+        {
+            if (StartSelfRun == true)
+                Play();
+        }
+
+        private void OnDisable()
+        {
+            Stop();
+        }
+
+        private void OnDestroy()
+        {
+            Stop();
+        }
+
+        public void Play()
+        {
+            Stop();
+
+            _isSelfRestart = false;
+            _timer = StartCoroutine(Timer());
+        }
+
+        public void Stop()
+        {
+            if (_timer != null)
+                StopCoroutine(_timer);
+        }
+
+        // сразу устанавливаем первый спрайт в рендерер и маску
+        public void SetFirstSprite()
+        {
+            _render.sprite = _sprites[0];
+            _mask.sprite = _masks[0];
+        }
+
+        IEnumerator Timer()
+        {
+            if(!_isSelfRestart)
+                yield return new WaitForSeconds(TimeDelay);
+
+            while (true)
+            {
+                yield return new WaitForSeconds(_speed / _sprites.Length);
+                AnimationUpdate();
+            }
+        }
+
+        private void AnimationUpdate()
+        {
+            _render.sprite = _sprites[index];
+            _mask.sprite = _masks[index];
+            index++;
+
+            if (index < _sprites.Length)
+                return;
+
+            //когда анимация подошла к концу
+
+            index = 0;
+
+            OnAnimationEnd();
+
+            if (Loop == true)
+            {
+                Stop();
+                _isSelfRestart = true;
+                _timer = StartCoroutine(Timer());
+            }
+            else
+                Stop();
+        }
+
+        protected virtual void OnAnimationEnd()
+        {
+            
+        }
+    }
+}
